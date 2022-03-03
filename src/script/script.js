c689b92769380a11d23b7f6b0e320a2f6634593f
Nazmus Sayad <online2sayad@gmail.com>
2022-03-03 23:28:37 +0600

    Update

--- src/script/script.js
+++ src/script/script.js
@@ -1,36 +1,61 @@
-const el = document.getElementById("a")
-let oldpos
-let width = -el.clientWidth
-let leftMove = width
+const slider = document.getElementById("slider"),
+  slider__main = document.getElementById("slider__main"),
+  slider__background = document.getElementById("slider__background"),
+  hide__slider = () => {
+    slider.className = "hide"
+    leftMove = width
+    setTimeout(() => {
+      slider.removeAttribute("class")
+    }, 210)
+  }
+
+let oldpos,
+  width = -slider__main.clientWidth,
+  leftMove = width
 
 document.addEventListener("pointermove", () => {
   const current = event.clientX
   if (event.buttons) {
     leftMove += current - oldpos
     leftMove = leftMove > 0 ? 0 : leftMove < width ? width : leftMove
-    el.style.left = leftMove + "px"
+
+    slider__main.style.left = leftMove + "px"
+    slider__background.style.opacity = 1 - leftMove / width
+
+    if (width - leftMove < -15) {
+      slider__background.style.display = "block"
+    }
   }
   oldpos = current
 })
+
 document.addEventListener("pointerdown", () => {
-  el.style.left = leftMove + "px"
-  el.removeAttribute("class")
+  if (leftMove === 0) {
+    slider__main.style.left = "0px"
+    slider__background.style.display = "block"
+    slider__background.style.opacity = 1
+  }
+  slider.removeAttribute("class")
 })
+
 document.addEventListener("pointerup", () => {
   if (leftMove < width / 2) {
-    el.className = "hide"
-    leftMove = width
+    hide__slider()
   } else {
-    el.className = "show"
+    slider.className = "show"
     leftMove = 0
   }
-  el.removeAttribute("style")
+  slider__main.removeAttribute("style")
+  slider__background.removeAttribute("style")
 })
+
 window.addEventListener("resize", () => {
-  width = -el.clientWidth
-  if (el.className == "show") {
+  width = -slider__main.clientWidth
+  if (slider.className == "show") {
     leftMove = 0
   } else {
     leftMove = width
   }
 })
+
+slider__background.addEventListener("click", hide__slider())
